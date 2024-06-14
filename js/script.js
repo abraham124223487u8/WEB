3317ee14b21b0096c7cba9c4393f338bf077b83c
abraham124223487u8 <abraham.deleon.v4@gmail.com>
2024-06-13 16:56:13 -1200

    Se agrega el Proyecto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function updateProgressBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+window.onscroll = function() {
+    updateProgressBar();
+};
